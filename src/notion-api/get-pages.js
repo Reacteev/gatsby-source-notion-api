72345fe29ceac8be0790e2fdd59b36f5a05c8383
Nicolas Morin <nicolas.morin@reacteev.com>
2022-02-21 17:43:22 +0100

    Do not fail if Notion databaseId not defined

--- src/notion-api/get-pages.js
+++ src/notion-api/get-pages.js
@@ -3,46 +3,46 @@ const { errorMessage } = require("../error-message")
 const { getBlocks } = require("./get-blocks")
 
 exports.getPages = async ({ token, databaseId, notionVersion = "2021-08-16", filter }, reporter) => {
-	let hasMore = true
-	let startCursor = ""
-	const url = `https://api.notion.com/v1/databases/${databaseId}/query`
-	const body = {
-		page_size: 100,
-	}
+  let hasMore = !!databaseId
+  let startCursor = ""
+  const url = `https://api.notion.com/v1/databases/${databaseId}/query`
+  const body = {
+    page_size: 100,
+  }
   if (filter) {
     body.filter = filter;
   }
 
-	const pages = []
+  const pages = []
 
-	while (hasMore) {
-		if (startCursor) {
-			body.start_cursor = startCursor
-		}
+  while (hasMore) {
+    if (startCursor) {
+      body.start_cursor = startCursor
+    }
 
-		try {
-			const result = await fetch(url, {
-				method: "POST",
-				body: JSON.stringify(body),
-				headers: {
-					"Content-Type": "application/json",
-					"Notion-Version": notionVersion,
-					Authorization: `Bearer ${token}`,
-				},
-			}).then((res) => res.json())
+    try {
+      const result = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          "Notion-Version": notionVersion,
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res) => res.json())
 
-			startCursor = result.next_cursor
-			hasMore = result.has_more
+      startCursor = result.next_cursor
+      hasMore = result.has_more
 
-			for (let page of result.results) {
-				page.children = await getBlocks({ id: page.id, token, notionVersion }, reporter)
+      for (let page of result.results) {
+        page.children = await getBlocks({ id: page.id, token, notionVersion }, reporter)
 
-				pages.push(page)
-			}
-		} catch (e) {
-			reporter.panic(errorMessage)
-		}
-	}
+        pages.push(page)
+      }
+    } catch (e) {
+      reporter.panic(errorMessage)
+    }
+  }
 
-	return pages
+  return pages
 }
